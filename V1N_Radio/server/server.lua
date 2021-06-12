a6f10a1ecca3e6bdac433bfe4a3fa60ff02c8452
V1NDs <65359820+V1NDs@users.noreply.github.com>
2021-06-12 18:24:17 +0200

    Update server.lua

--- V1N_Radio/server/server.lua
+++ V1N_Radio/server/server.lua
@@ -28,6 +28,10 @@ local vRP = Proxy.getInterface("vRP")
 local cfg = module(GetCurrentResourceName(), "cfg/config")
 --===================================--
 
+--==Variables==--
+local access = nil
+--===================================--
+
 --==Events==--
 RegisterServerEvent("V1N_Radio:checkRank", function(channel)
     local source = source
@@ -38,11 +42,24 @@ RegisterServerEvent("V1N_Radio:checkRank", function(channel)
         for k,v in pairs(cfg.permissions[channel]) do
             if jobs["groups"][v] ~= nil then
                 TriggerClientEvent("V1N_Radio:addToChannel", source, user_id, channel)
+                access = true
                 return
             end
         end
+
+        access = false
     else
         TriggerClientEvent("V1N_Radio:addToChannel", source, user_id, channel)
+    end
+
+    if not access then
+        if cfg.notification ~= "" then
+            if cfg.notification == "pNotify" then
+                TriggerClientEvent("pNotify:SendNotification", source, { text = "Du har ikke adgang til radio kanal "..channel, type = "error", queue = "global", timeout = 4000, layout = "centerRight", animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"} })
+            else
+                TriggerClientEvent("mythic_notify:client:SendAlert", source, { type = "error", text = "Du har ikke adgang til radio kanal "..channel, duration = 4000})
+            end
+        end
     end
 end)
 --===================================--
